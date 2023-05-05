92a33b5d8728108574ee74c356aa4d7a43e7ca26
tameralshazly <tameralshazly@yahoo.com>
2023-05-05 06:41:46 +0400

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notly/constants/routes.dart';
 import 'package:notly/views/login_view.dart';
 import 'package:notly/views/register_view.dart';
 import 'package:notly/views/notes_view.dart';
@@ -17,9 +18,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       },
     ),
   );
