077dc995bddaa8b8440552c5511f8614023f9c46
tameralshazly <tameralshazly@yahoo.com>
2023-06-08 18:42:09 +0400

    Step-13

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notly/constants/routes.dart';
 import 'package:notly/services/auth/auth_service.dart';
 import 'package:notly/views/login_view.dart';
+import 'package:notly/views/notes/new_note_view.dart';
 import 'package:notly/views/register_view.dart';
-import 'package:notly/views/notes_view.dart';
+import 'package:notly/views/notes/notes_view.dart';
 import 'package:notly/views/verify_email_view.dart';
 
 void main() {
@@ -20,6 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notly/constants/routes.dart';
 import 'package:notly/services/auth/auth_service.dart';
 import 'package:notly/services/crud/notes_service.dart';
-import '../enums/menu_action.dart';
+import '../../enums/menu_action.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -31,8 +31,14 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main UI'),
+        title: const Text('Your Notes'),
         actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(newNoteRoute);
+            },
+            icon: const Icon(Icons.add),
+          ),
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {
               switch (value) {
